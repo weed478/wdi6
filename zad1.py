5cf53f6762ff6b6cca2d99388989b2fd59ef86c0
Carbon <carbon.szlachta@gmail.com>
2020-11-25 16:50:40 +0100

    zad1 v2 wip

--- zad1.py
+++ zad1.py
@@ -1,6 +1,3 @@
-end = None
-
-
 def is_prime(n):
     if n < 2:
         return False
@@ -8,46 +5,30 @@ def is_prime(n):
         return True
     elif n % 2 == 0:
         return False
-    end
 
     div = 3
     while div*div <= n:
         if n % div == 0:
             return False
-        end
         div += 2
-    end
+
     return True
 
 
-checked = set()
+def rebuild(num, mask):
+    new_num = 0
+    new_num_len = 0
+
+    while mask > 0:
+        mask, bit = divmod(mask, 2)
+        num, digit = divmod(num, 10)
+
+        if bit == 1:
+            new_num += digit * 10 ** new_num_len
+            new_num_len += 1
+
+    return new_num
 
 
-def cut_digit(num):
-    a = num
-    b = 0
-    c = 0
-    c_len = 0
-
-    while a > 0:
-        c += b * 10**c_len // 10
-        c_len += 1
-        b = a % 10
-        a //= 10
-        cut_num = c + a * 10**c_len // 10
-
-        if cut_num in checked:
-            continue
-
-        if is_prime(cut_num):
-            print(cut_num)
-
-        if cut_num > 99:
-            cut_digit(cut_num)
-
-        checked.add(cut_num)
-    end
-
-
-cut_digit(1234567)
+print(rebuild(1234567000, 0b0101011111))
 
