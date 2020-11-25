66ec105808f4b25cf112008bfa9e82244b285e48
Carbon <carbon.szlachta@gmail.com>
2020-11-25 20:50:29 +0100

    ultimyszka

--- zad1.py
+++ zad1.py
@@ -1,3 +1,25 @@
+def myszka(num, min_len, min_cut):
+    def ultimyszka(number=0, pos=0, depth=0):
+        space_after = min_cut - 1 - depth
+        for a in range(pos, len(num_tab) - max(0, space_after)):
+            new_number = number * 10 + num_tab[len(num_tab) - a - 1]
+            if depth >= min_len - 1:
+                yield new_number
+
+            if space_after >= 0 or a + 1 < len(num_tab):
+                if depth + 1 < len(num_tab) - min_cut:
+                    for i in ultimyszka(new_number, a + 1, depth + 1):
+                        yield i
+
+    num_tab = []
+    while num > 0:
+        num, r = divmod(num, 10)
+        num_tab.append(r)
+
+    if len(num_tab) > 0 and len(num_tab) >= min_len > 0 and len(num_tab) - min_len >= min_cut >= 0:
+        return ultimyszka()
+
+
 def is_prime(n):
     if n < 2:
         return False
@@ -11,24 +33,8 @@ def is_prime(n):
         if n % div == 0:
             return False
         div += 2
-
     return True
 
 
-def rebuild(num, mask):
-    new_num = 0
-    new_num_len = 0
-
-    while mask > 0:
-        mask, bit = divmod(mask, 2)
-        num, digit = divmod(num, 10)
-
-        if bit == 1:
-            new_num += digit * 10 ** new_num_len
-            new_num_len += 1
-
-    return new_num
-
-
-print(rebuild(1234567000, 0b0101011111))
-
+for prime in filter(is_prime, myszka(12345, 2, 1)):
+    print(prime, end=", ")
