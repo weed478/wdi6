a4ac5df95867d12e3d17ec2ce0f85cd91087b594
Carbon <carbon.szlachta@gmail.com>
2020-11-25 21:34:24 +0100

    carbonowe2

--- zad2.py
+++ zad2.py
@@ -1,29 +1,49 @@
-def waga(n):
-    div = 2
+def waga(num):
+    w = 0
 
-    num_factorials = 0
-    last_div = 0
+    while num > 1:
+        f = 2
+        while f <= num:
+            if num % f == 0:
+                while num % f == 0:
+                    num //= f
+                w += 1
+            f += 1
 
-    while n > 1 and div ** 2 <= n:
-        while n % div == 0:
-            n /= div
-            if last_div != div:
-                num_factorials += 1
-                last_div = div
-        div += 1
-
-    return num_factorials
+    return w
 
 
-def czy_3_z_tszy_da_3(tszy):
-    sum = 0
-    for n in tszy:
-        sum += waga(n)
+def next(p):
+    p[len(p) - 1] += 1
+    for i in range(len(p) - 2, -1, -1):
+        if p[i + 1] >= 3:
+            p[i + 1] = 0
+            p[i] += 1
 
-    return True if sum % 3 == 0 else False
+
+def works(tab, podzial):
+    s = [0, 0, 0]
+    for i in range(len(tab)):
+        s[podzial[i]] += tab[i]
+
+    return s[0] == s[1] == s[2]
 
 
-tszy = [1, 54, 42, 15, 2, 14, 9, 384]
-tszy2 = [2, 3, 5]
+def zad2(tab):
+    tab = [waga(n) for n in tab]
+    podzial = [0 for _ in tab]
 
-print(czy_3_z_tszy_da_3(tszy))
+    while podzial[0] < 3:
+        if works(tab, podzial):
+            return True
+        next(podzial)
+
+    return False
+
+
+from random import shuffle
+
+data = [64, 6, 30, 1, 1, 2, 2, 2]
+shuffle(data)
+
+print(zad2(data))
