4b8c82dd70673d7ec5ec7a2e2e3a97dd2287bd54
kappa243 <edek15pl@gmail.com>
2020-12-10 20:58:59 +0100

    fixes

--- zad16.py
+++ zad16.py
@@ -1,39 +1,40 @@
 from meszstack import meszgen
 
+# counts vowels in string
+def vowels(text):
+    count = 0
+    for letter in text:
+        for a in "aeiouy":
+            if letter == a:
+                count += 1
+    return count
 
+
+# return (sum of ascii char's dec value, vowels in string)
+def weight(text):
+    asc_sum = 0
+    for a in text:
+        asc_sum += ord(a)
+    return asc_sum, vowels(text)
+
+
+# return word or None if not found
 def wyraz(s1, s2=""):
-
-    # counts vowels in string
-    def vowels(text):
-        count = 0
-        for letter in text:
-            for a in "aeiouy":
-                if letter == a:
-                    count += 1
-        return count
-
-    # return (sum of ascii char's dec value, vowels in string)
-    def weight(text):
-        asc_sum = 0
-        for a in text:
-            asc_sum += ord(a)
-        return asc_sum, vowels(text)
-
     s1_weight = weight(s1)
     s2_weight = weight(s2)
 
     if s1_weight[1] > 0:  # if vowels exist in s1
         if s2_weight[1] < s1_weight[1]:  # if s2 has less vowels than s1 we cannot make a subset that has the same number of vowels
-            return False
+            return None
 
     for subset in meszgen(s2):
         text = ""
         for n in subset:
             text += s2[n]
         if weight(text) == s1_weight:
-            return True
+            return text
 
-    return False
+    return None
 
 
-print(wyraz("testxdd", "alahehe"))
+print(wyraz("testxdd", "toastxdde"))
--- zad17.py
+++ zad17.py
@@ -2,35 +2,25 @@ from is_prime import is_prime
 
 
 def zad17(a,b):
-
-    def num_len(n):
-        if n == 0:
-            return 1
-
-        c = 0
-        while n > 0:
-            n, rem = divmod(n, 10)
-            c += 1
-        return c
-
     def build(n1, n2, num=0, l=0):
-        def next(a,b):
+        def nexte(a, b):
             if a != -1:
                 _num, _rem = divmod(a, 10)
                 if _num == 0:
                     _num = -1
-                _rem = _rem * (10**l) + num
-                yield from build(_num, b, _rem, l+1)
+                _rem = _rem * (10 ** l) + num
+                yield from build(_num, b, _rem, l + 1)
 
-        yield from next(n1,n2)
-        yield from next(n2,n1)
+        yield from nexte(n1, n2)
+        yield from nexte(n2, n1)
 
         if n1 == n2 == -1:
             yield num
 
     count = 0
-    for _ in filter(is_prime, build(a,b)):
-        count +=1
+    for _ in filter(is_prime, build(a, b)):
+        count += 1
     return count
 
-print(zad17(11,21))
+
+print(zad17(11, 21))
