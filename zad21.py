b65c7672e3d18066fddc7c8d2b3d731fcf4a0160
Carbon <carbon.szlachta@gmail.com>
2020-12-10 21:53:21 +0100

    zad21 with meszstack

--- zad21.py
+++ zad21.py
@@ -6,18 +6,44 @@ def zad21(t, s):
 
     def next_i(i=None):
         if i is None:
-            return 0
+            return 0, 0
 
-        elif i + 1 < n*n:
-            return i + 1
+        elif i[1] + 1 < n:
+            return i[0], i[1] + 1
+
+        elif i[0] + 1 < n:
+            return i[0] + 1, 0
 
         else:
             return None
 
-    for x in meszgen(next_i):
-        print(list(map(lambda i: (i % n, i // n), x)))
+    for pos in meszgen(next_i):
+        cols = set()
+        rows = set()
+
+        current_sum = 0
+
+        for p in pos:
+            if p[0] in rows:
+                break
+
+            rows.add(p[0])
+
+            if p[1] in cols:
+                break
+
+            cols.add(p[1])
+
+            current_sum += t[p[0]][p[1]]
+
+        if current_sum == s:
+            print(pos)
+            return True
+
+    return False
 
 
-tab = [[0, 1],
-       [1, 0]]
-zad21(tab, 2)
+tab = [[0, 1, 0],
+       [1, 0, 0],
+       [0, 0, 1]]
+print(zad21(tab, 3))
